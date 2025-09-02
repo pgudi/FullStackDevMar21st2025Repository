77524eb2d5fd68ba2ca8df9ffd18ab31f741a792
pgudi <prabhakargudi@gmail.com>
2025-09-02 10:27:01 +0530

    Aspect Oriented Programming

---
+++ spring-deaprtmentnew/src/main/java/com/gentech/dept/entity/Department.java
@@ -0,0 +1,58 @@
+package com.gentech.dept.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tbl_department")
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "department_no")
+    private Integer id;
+
+    @Column(name = "department_name")
+    private String dname;
+
+    @Column(name = "department_location")
+    private String loc;
+
+    public Department(){
+
+    }
+
+    public Department(String dname, String loc) {
+        this.dname = dname;
+        this.loc = loc;
+    }
+
+    public Department(Integer id, String dname, String loc) {
+        this.id = id;
+        this.dname = dname;
+        this.loc = loc;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setDname(String dname) {
+        this.dname = dname;
+    }
+
+    public void setLoc(String loc) {
+        this.loc = loc;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getDname() {
+        return dname;
+    }
+
+    public String getLoc() {
+        return loc;
+    }
+}
